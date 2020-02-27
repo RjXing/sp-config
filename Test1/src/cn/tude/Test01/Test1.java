1787d5b11c05980d5fcd09e7c86d4cf8609cd205
renjiaxing <renjiaxing@192.168.70.1>
2020-02-27 17:42:08 +0800

    456

--- Test1/src/cn/tude/Test01/Test1.java
+++ Test1/src/cn/tude/Test01/Test1.java
@@ -3,7 +3,7 @@ public class Test1 {
     public static void main(String[] args) {
 		System.out.println(1);
 		System.out.println(2);
-		//111111111111
-		//222222222
+		//1111111
+		//222222222222
 	}
 }
